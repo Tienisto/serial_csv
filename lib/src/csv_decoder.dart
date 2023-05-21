552e8e7de71587fe5052d56f70a50539e39acb36
Tien Do Nam <dev.tien.donam@gmail.com>
2023-05-21 15:26:11 +0200

    feat: improve performance even more

--- lib/src/csv_decoder.dart
+++ lib/src/csv_decoder.dart
@@ -1,8 +1,27 @@
 const _asciiLineBreak = 10; // \n
 const _asciiQuote = 34; // "
 const _asciiComma = 44; // ,
+const _asciiDot = 46; // .
 const _asciiT = 116; // t
 const _asciiF = 102; // f
+const _ascii0 = 48; // 0
+const _ascii1 = 49; // 1
+const _ascii2 = 50; // 2
+const _ascii3 = 51; // 3
+const _ascii4 = 52; // 4
+const _ascii5 = 53; // 5
+const _ascii6 = 54; // 6
+const _ascii7 = 55; // 7
+const _ascii8 = 56; // 8
+const _ascii9 = 57; // 9
+
+enum _DataType {
+  string,
+  integer,
+  double,
+  boolean,
+  nullValue,
+}
 
 class SerialCsvDecoder {
   const SerialCsvDecoder();
@@ -13,8 +32,9 @@ class SerialCsvDecoder {
     final List<List<Object?>> parsedData = [];
 
     List<Object?> currentRow = [];
-    bool inQuotes = false;
-    bool hadQuotes = false;
+    _DataType currentType = _DataType.nullValue; // expect null if empty
+    bool boolValue = false;
+    bool finishedString = false;
     List<int> currentCell = [];
 
     for (int i = 0; i < bytes.length; i++) {
@@ -22,115 +42,166 @@ class SerialCsvDecoder {
 
       switch (c) {
         case _asciiQuote:
-          if (inQuotes) {
+          if (currentType == _DataType.string) {
             if (i + 1 < bytes.length && bytes[i + 1] == _asciiQuote) {
               // Escaped quote
               currentCell.add(_asciiQuote);
               i++; // skip the second quote
             } else {
-              // End of quoted field
-              inQuotes = false;
-              hadQuotes = true;
+              // we are done with the string
+              // expecting a comma or line break next
+              finishedString = true;
             }
           } else {
-            // Start of quoted field
-            inQuotes = true;
+            // detect as string
+            currentType = _DataType.string;
           }
           break;
         case _asciiComma:
         case _asciiLineBreak:
-          // end of cell or row
-          if (inQuotes) {
+          if (currentType == _DataType.string && !finishedString) {
+            // we are still in the string
             currentCell.add(c);
           } else {
-            currentRow.add(
-              _decodeCell(String.fromCharCodes(currentCell), hadQuotes),
-            );
+            // end of cell or row
+            switch (currentType) {
+              case _DataType.string:
+                currentRow.add(String.fromCharCodes(currentCell));
+                break;
+              case _DataType.integer:
+                currentRow.add(int.parse(String.fromCharCodes(currentCell)));
+                break;
+              case _DataType.double:
+                currentRow.add(double.parse(String.fromCharCodes(currentCell)));
+                break;
+              case _DataType.boolean:
+                currentRow.add(boolValue);
+                break;
+              case _DataType.nullValue:
+                currentRow.add(null);
+                break;
+            }
+
+            // reset cell
             currentCell = [];
-            hadQuotes = false;
+            finishedString = false;
+            currentType = _DataType.nullValue;
 
             if (c == _asciiLineBreak) {
-              // End of the row
+              // end of the row
               parsedData.add(currentRow);
+
+              // reset row
               currentRow = [];
             }
           }
           break;
         default:
-          currentCell.add(c);
+          switch (currentType) {
+            case _DataType.string:
+              currentCell.add(c);
+              break;
+            case _DataType.integer:
+              currentCell.add(c);
+              if (c == _asciiDot) {
+                // change to double
+                currentType = _DataType.double;
+              }
+              break;
+            case _DataType.double:
+              currentCell.add(c);
+              break;
+            case _DataType.boolean:
+              // we already detected the boolean value on the first character
+              break;
+            case _DataType.nullValue:
+              // no type detected yet
+              // detect type on first character
+              switch (c) {
+                case _asciiT:
+                case _asciiF:
+                  currentType = _DataType.boolean;
+                  boolValue = c == _asciiT; // we are finished with the boolean
+                  break;
+                case _ascii0:
+                case _ascii1:
+                case _ascii2:
+                case _ascii3:
+                case _ascii4:
+                case _ascii5:
+                case _ascii6:
+                case _ascii7:
+                case _ascii8:
+                case _ascii9:
+                  // assume as integer first, we change to double if we see a dot
+                  currentType = _DataType.integer;
+                  currentCell.add(c);
+                  break;
+                default:
+                  currentType = _DataType.string;
+                  currentCell.add(c);
+                  break;
+              }
+              break;
+          }
       }
     }
 
     return parsedData;
   }
 
-  Object? _decodeCell(String raw, bool hadQuotes) {
-    if (hadQuotes) {
-      // a string
-      return raw;
-    } else if (raw.isEmpty) {
-      // a null value
-      return null;
-    } else if (raw.codeUnitAt(0) == _asciiT) {
-      // a 'true' boolean
-      return true;
-    } else if (raw.codeUnitAt(0) == _asciiF) {
-      // a 'false' boolean
-      return false;
-    } else if (raw.contains('.')) {
-      // a double
-      return double.parse(raw);
-    } else {
-      // an integer
-      return int.parse(raw);
-    }
-  }
-
   /// Decodes a CSV string into a list of rows (specialized for strings).
   List<List<String>> decodeStringList(String data) {
+    final bytes = data.codeUnits;
     final List<List<String>> parsedData = [];
 
     List<String> currentRow = [];
-    bool inQuotes = false;
-    final buffer = StringBuffer();
+    bool inString = false;
+    List<int> currentCell = [];
 
-    for (int i = 0; i < data.length; i++) {
-      int c = data.codeUnitAt(i);
+    for (int i = 0; i < bytes.length; i++) {
+      int c = bytes[i];
 
       switch (c) {
         case _asciiQuote:
-          if (inQuotes) {
-            if (i < data.length - 1 && data.codeUnitAt(i + 1) == _asciiQuote) {
+          if (inString) {
+            if (i + 1 < bytes.length && bytes[i + 1] == _asciiQuote) {
               // Escaped quote
-              buffer.write('"');
+              currentCell.add(_asciiQuote);
               i++; // skip the second quote
             } else {
-              // End of quoted field
-              inQuotes = false;
+              // we are done with the string
+              // expecting a comma or line break next
+              inString = false;
             }
           } else {
-            // Start of quoted field
-            inQuotes = true;
+            inString = true;
           }
           break;
         case _asciiComma:
         case _asciiLineBreak:
-          // end of cell or row
-          if (inQuotes) {
-            buffer.writeCharCode(c);
+          if (inString) {
+            // we are still in the string
+            currentCell.add(c);
           } else {
-            currentRow.add(buffer.toString());
-            buffer.clear();
+            // end of cell or row
+            currentRow.add(String.fromCharCodes(currentCell));
+
+            // reset cell
+            currentCell = [];
+            inString = false;
 
             if (c == _asciiLineBreak) {
-              // End of the row
+              // end of the row
               parsedData.add(currentRow);
+
+              // reset row
               currentRow = [];
             }
           }
           break;
         default:
-          buffer.writeCharCode(c);
+          currentCell.add(c);
       }
     }
 
